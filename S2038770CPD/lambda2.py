2ce0ba648d176d879fd368a63c58f790a1b1cf9d
Benson Mugure <b.mugure@alustudent.com>
2024-04-07 19:58:44 +0400

    add link to image in email notif

--- S2038770CPD/lambda2.py
+++ S2038770CPD/lambda2.py
@@ -5,6 +5,7 @@ from datetime import datetime
 # Initialize the DynamoDB and SNS clients
 dynamodb = boto3.client('dynamodb')
 sns = boto3.client('sns')
+s3 = boto3.client('s3')
 
 # Define the DynamoDB table name
 vehicle_table_name = 'vehicleTableS2038770'
@@ -13,18 +14,17 @@ sns_topic_arn = 'arn:aws:sns:us-east-1:519012545046:Vehicles-Notification'
 current_time = datetime.now()
 formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
-def send_email(subject, message, topic_arn=sns_topic_arn):
-    """Send an email using the SNS service
-    :param subject: Email subject
-    :param message: Email message
-    :param topic_arn: SNS topic ARN
-    :return: True if successful, False otherwise
-    """
+def send_email(subject, message, link=None, topic_arn=sns_topic_arn):
     try:
+        email_message = message
+        if link:
+            # If link is provided, include the link in the email
+            email_message += f'\n\nDownload the Image with the Vehicle here: {link}'
+
         response = sns.publish(
             TopicArn=topic_arn,
             Subject=subject,
-            Message=message
+            Message=email_message
         )
         print("Email sent successfully:", response)
         return True
@@ -43,6 +43,7 @@ def lambda_handler(event, context):
 
     try:
         # Extract label names
+        print(f'Event: {event}')
         label_names = event['Records'][0]['dynamodb']['NewImage']['label_names']['L'] or event['Records'][0]['dynamodb']['OldImage']['label_names']['L']
         label_names = [item['S'] for item in label_names]
 
@@ -54,8 +55,10 @@ def lambda_handler(event, context):
             }
         
         # Extract list of text values from the DynamoDB event
-        new_text_values = event['Records'][0]['dynamodb']['NewImage']['text_values']['L']
+        new_text_values = event['Records'][0]['dynamodb']['NewImage']['text_values']['L'] # or event['Records'][0]['dynamodb']['OldImage']['text_values']['L']
         new_text_values = [item['S'] for item in new_text_values]
+        
+        print(f'New Text Values: {new_text_values}')
 
         # Retrieve text values from the vehicleTableS2038770 DynamoDB table
         response = dynamodb.scan(
@@ -75,14 +78,18 @@ def lambda_handler(event, context):
 
         # Check if any new text value can be found in the vehicle table text values
         if not new_text_values_set.intersection(vehicle_table_text_values_set):
+            # ! send an SNS notification
             print(f'No matching text values found in the vehicle table for image: {event["Records"][0]["dynamodb"]["NewImage"]["imageName"]["S"]}')
-    
+            
             email_subject = 'Unidentified Vehicle Detected'
-            email_message = f'An unidentified vehicle was detected in the image: {event["Records"][0]["dynamodb"]["NewImage"]["imageName"]["S"]}\n'
-            email_message += f'Event Time: {event["Records"][0]["dynamodb"]["NewImage"]["eventTime"]["S"]}\n'
-            email_message += f'Text Detected: {new_text_values}\n'
-            email_message += f'Vehicle Labels: {label_names}'
-            send_email(email_subject, email_message)
+            email_message = f'\nAn unidentified vehicle was detected in the image: {event["Records"][0]["dynamodb"]["NewImage"]["imageName"]["S"]}\n'
+            email_message += f'\nEvent Time: {event["Records"][0]["dynamodb"]["NewImage"]["eventTime"]["S"]}\n'
+            email_message += f'\nText Detected: {new_text_values}\n'
+            email_message += f'\nVehicle Labels: {label_names}'
+            # Set the S3 link to the image
+            s3_link = f'https://{event["Records"][0]["dynamodb"]["NewImage"]["image_bucket"]["S"]}.s3.amazonaws.com/{event["Records"][0]["dynamodb"]["NewImage"]["imageName"]["S"]}'
+            print(f'S3 Link: {s3_link}')
+            send_email(email_subject, email_message, link=s3_link)
         else:
             # Check if any text value is blacklisted - if there is an intersection from the above
             response = dynamodb.scan(
@@ -100,13 +107,17 @@ def lambda_handler(event, context):
             
             # Check if any new text value matches a blacklisted value
             if new_text_values_set.intersection(blacklisted_text_values_set):
+                # ! Send email via SNS
                 print(f'Blacklisted text value detected in an image')
                 
                 email_subject = 'Blacklisted Vehicle Detected'
-                email_message = f'A blacklisted Vehicle value was detected in an image\n'
-                email_message += f'Event Time: {formatted_time}\n'
-                email_message += f'Vehicle Number Plate: {blacklisted_text_values_set}\n'
-                send_email(email_subject, email_message)
+                email_message = f'A blacklisted Vehicle value was detected in the image: {event["Records"][0]["dynamodb"]["NewImage"]["imageName"]["S"]}\n'
+                email_message += f'\nEvent Time: {formatted_time}\n'
+                email_message += f'\nVehicle Number Plate: {blacklisted_text_values_set}\n'
+                # Set the S3 link to the image
+                s3_link = f'https://{event["Records"][0]["dynamodb"]["NewImage"]["image_bucket"]["S"]}.s3.amazonaws.com/{event["Records"][0]["dynamodb"]["NewImage"]["imageName"]["S"]}'
+                print(f'S3 Link: {s3_link}')
+                send_email(email_subject, email_message, link=s3_link)
             else:
                 print('No Blacklisted image uploaded')
 
@@ -116,7 +127,7 @@ def lambda_handler(event, context):
         }
     except Exception as e:
         print(f'Error processing event: {e}')
+        print(f'Event: {event}')
         return {
             'statusCode': 500,
-            'body': json.dumps('Failed to process event')
         }
